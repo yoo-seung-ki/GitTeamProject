b384dfd4fed2a4a825601238e07d444f867d6b2c
류승기 <ysgsl@naver.com>
2023-03-10 19:32:38 +0900

    Remainfunc 변경

--- GitTeamProj/src/main/java/org/example/calcurator/Remainfunc.java
+++ GitTeamProj/src/main/java/org/example/calcurator/Remainfunc.java
@@ -1,4 +1,17 @@
 package org.example.calcurator;
 
 public class Remainfunc {
+
+  int left;
+  int right;
+
+  public Remainfunc(int left, int right) {
+    this.left = left;
+    this.right = right;
+  }
+
+  public int result() {
+
+    return left % right ; 
+  }
 }
